d6d396948d0cb3415d2505101496a4d2c2b9d623
Ivan6274 <Isivanov2011@gmail.com>
2020-08-23 15:43:15 +0300

    Bonus app added

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+int balans = (int)100F;
+int deposit = 145;
+int i = 100;
+int bonus = deposit/i;
+if (deposit >= 1000) {deposit = deposit + bonus;}
+int totalBalans = balans + deposit;
+System.out.println(totalBalans);
+
+
+    }
+}
