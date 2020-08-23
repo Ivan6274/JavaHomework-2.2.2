9ffc2d90b57f73b4faf5d7dff0eb279228a4869e
Ivan6274 <Isivanov2011@gmail.com>
2020-08-23 16:20:28 +0300

    Fix app

--- Main.java
+++ Main.java
@@ -3,10 +3,12 @@ public class Main {
 int balans = (int)100F;
 int deposit = 145;
 int i = 100;
-int bonus = deposit/i;
-if (deposit >= 1000) {deposit = deposit + bonus;}
+int bonus;
+if (deposit >= 1000) {bonus = deposit/i;}
+else {bonus = 0;}
 int totalBalans = balans + deposit;
-System.out.println(totalBalans);
+System.out.println( "Бонус = " + bonus);
+System.out.println("Баланс итого = " + totalBalans);
 
 
     }
